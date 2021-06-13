c85dd64aa5457063aa147d5cf1b656011a374f04
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-06-13 10:48:47 +0700

    use bcrypt to secure data as password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,13 +1,16 @@
+const bcrypt = require("bcrypt");
 const { response } = require("../helpers/standardResponse");
 const { createUsers } = require(`../models/users`);
 
-exports.register = (req, res) => {
-  createUsers(req.body, (error) => {
+exports.register = async (req, res) => {
+  const data = req.body;
+  data.password = await bcrypt.hash(data.password, await bcrypt.genSalt());
+  createUsers(data, (error) => {
     if (!error) {
       return response(res, 200, true, "Register successfully!");
     } else {
       console.log(error);
-      return response(res, 200, true, "Register successfully!");
+      return response(res, 500, true, "Register failed!");
     }
   });
 };
